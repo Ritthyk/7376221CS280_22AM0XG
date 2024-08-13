ce9a7ac1f30122ab86ff3031d15a566cc527e340
Ritthyk <118051599+Ritthyk@users.noreply.github.com>
2024-08-13 15:42:18 +0530

    Create app.py

---
+++ DAY#1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!!"
+
+@app.route('/getDetails')
+def getDetails():
+    return "Ritthyk M - 7376221CS280"
+
+if __name__=='__main__':
+    app.run(debug=True)
